f3d8aa718e62ca21cefe1c81a3c63558e6b202e8
thejennywang <thejennywang@gmail.com>
2015-01-23 16:04:32 -0800

    Contents switched to array instead of hash

--- lib/board.rb
+++ lib/board.rb
@@ -3,23 +3,19 @@ class Board
   attr_accessor :contents
 
   def initialize
-    @contents = {}
-    (1..9).each do |number|
-      @contents["#{number}"] = ""
-    end
-    @contents
+    @contents = Array.new(10)
   end
 
   def place(piece, position)
-    if @contents["#{position}"] == "" 
-      @contents["#{position}"] = "#{piece}"
+    if @contents[position] == nil 
+      @contents[position] = "#{piece}"
     else
       raise "This square is taken."
     end
   end
 
   def full?
-    @contents.has_value?("") == false
+    @contents.all? { |obj| obj.is_a? String }
   end
 
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -5,17 +5,15 @@ describe 'Board' do
   
   let (:board) { Board.new }
 
-  it 'is created as a hash with numbered keys and empty values' do
+  it 'is created as an empty array of moves' do
 
-    expect(board.contents).to eq("1" => "", "2" => "", "3" => "", "4" => "", "5" => "", "6" => "", "7" => "", "8" => "", "9" => "")
+    expect(board.contents).to eq([nil, nil, nil, nil, nil, nil, nil, nil, nil, nil])
   end
 
   it 'knows when it is full' do
     expect(board.full?).to be false
 
-    board.contents.each_value do |value|
-      value.replace "x"
-    end
+    board.contents.fill("x")
 
     expect(board.full?).to be true
   end
@@ -26,7 +24,7 @@ describe 'Board' do
 
     board.place(player.id, position)
 
-    expect(board.contents["1"]).to eq "x"
+    expect(board.contents[position]).to eq "x"
     expect( lambda { board.place(player.id, position) } ).to raise_error(RuntimeError)
   end
 
